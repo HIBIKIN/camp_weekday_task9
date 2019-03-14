bd47e6c00f79a662f7f278963c9e3893035354c9
HIBIKIN <hi3bi1ki5@gmail.com>
2019-03-14 11:51:49 +0900

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/show'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
+  get 'tasks/index'
+  get 'tasks/hide'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
